87559530c98d5c842e5b9ee6521e4c3bed5fcbf5
obabichev <babichev.oleg.n@gmail.com>
2019-11-02 09:48:57 +0100

    1 lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Name name="Oleg" abc={123} u={new Date()}/>
+            <Name name="Oleg2"/>
+            <Name name="Oleg4"/>
+            <Name name="Oleg6"/>
+            <Name name="Oleg8"/>
+            <Name/>
+        </div>
+    );
+}
+
+// <Name name="Oleg" />
+// ===
+// React.createElement(
+//     Name,
+//     {
+//         name: 'Oleg'
+//     }
+// );
+
+function Name(props) {
+
+    console.log('[obabichev] props', props);
+
+    // const name = 'Your name';
+
+    return React.createElement(
+        'div',
+        {},
+        ['Name:', props.name]
+    )
+
+    // return <div>
+    //     Name: {props.name}
+    // </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,60 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const css = {
+    name: "your-generated-class-name"
+};
+
+/*
+//Equivalents
+
+const el = <div>
+    <a href="http://google.com">This is link</a>
+</div>;
+
+const ell = React.createElement(
+    'div',
+    {},
+    React.createElement(
+        'a',
+        {
+            href: 'http://google.com'
+        },
+        'This is link'
+    )
+);*/
+
+/*
+Fill the difference
+
+React.createElement(
+    App
+);
+
+React.createElement(
+    "div"
+);*/
+
+/*
+console.log('[obabichev] a', <div>Hallo</div>);
+{
+    $$typeof: Symbol(react.element)
+    key: null
+    props: {children: "Hallo"}
+    ref: null
+    type: "div"
+    _owner: null
+    _store: {validated: false}
+    _self: null
+    _source: {fileName: "/Users/obabichev/tmp/test-project/test-app/src/index.js", lineNumber: 41}
+    __proto__: Object
+}
+*/
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
